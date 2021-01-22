cc4ce27e4365c3da1a5795e334463a84254075e4
iinn <zhangdan@gongchang.com>
2021-01-22 13:46:43 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,15 @@
 const { task, series, watch } = require('gulp');
-
 const clean = require('./tasks/util/clean');
-
 const css = require('./tasks/build/css');
 const js = require('./tasks/build/javascript');
 const assets = require('./tasks/build/assets');
 const html = require('./tasks/build/html');
-const lib = require('./tasks/build/lib');
-
 const rev = require('./tasks/rev/rev');
-
-const serve = require('./tasks/serve/server').sv;
-
 const index = require('./tasks/plugin/gulp-index');
+const serve = require('./tasks/serve/server').sv;
+const config = require('./tasks/config');
+
+const { paths } = config;
 
 task('clean', clean);
 
@@ -20,35 +17,49 @@ task('css', css);
 
 task('js', js);
 
-task('lib', lib);
+task('assets', assets);
 
-task('assets', (cb) => { return assets(cb); });
+task('html', html);
 
-task('html', () => { return html(); });
+task('rev', rev);
 
-task('rev', () => { return rev(); });
-
-task('index', (cb) => { return index(cb); });
+task('index', index);
 
 task('serve', serve);
 
 // NODE_ENV=development gulp dev -m global/common
 exports.dev = series(['clean'], ['css', 'js', 'html'], ['index']);
 
+// NODE_ENV=production gulp build -m global/common
 exports.build = series(['clean'], ['css', 'js', 'html', 'rev']);
 
 exports.watch = series(
   serve,
   function (done) {
-    watch('src/**/src/*.+(js|es6)').on('change', function (file) {
-      js(file);
+    watch(paths.js).on('change', js);
+
+    watch(paths.js).on('unlink', clean);
+
+    watch(paths.css).on('change', css);
+
+    watch(paths.css).on('unlink', clean);
+
+    watch(paths.html).on('change', html);
+
+    watch(paths.html).on('add', (file) => {
+      series(() => html(file), ['index'])();
     });
-    watch('src/**/src/*.+(scss|css)').on('change', function (file) {
-      css(file);
+
+    watch(paths.html).on('unlink', function (file) {
+      series(() => clean(file), ['index'])();
     });
-    watch('src/**/html/*.html').on('change', function (file) {
-      html(file);
-    });
+    
+    watch(paths.assets).on('change', assets);
+
+    watch(paths.assets).on('add', assets);
+
+    watch(paths.assets).on('unlink', clean);
+
     done();
   }
 );
