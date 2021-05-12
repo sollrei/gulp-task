0477c75ca5de17d6b1f2bfc078631a55ed1621ee
iinn <zhangdan@gongchang.com>
2021-05-12 10:11:55 +0800

    change

--- tasks/util/clean.js
+++ tasks/util/clean.js
@@ -2,7 +2,7 @@ const del = require('del');
 const config = require('../config');
 
 module.exports = function (file) {
-  if (file) {
+  if (file && typeof file === 'string') {
     const dist = config.paths.dist;
     const filePath = file.replace('src/', dist);
     return del([filePath]);
